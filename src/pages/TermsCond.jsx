5d114befcead5cf0099c9bc80c4ae827c3aea822
Rodrigo Martínez Vallejo <rodrigo.martinezvall@gmail.com>
2025-09-22 12:58:19 -0600

    base for T&C

--- src/pages/TermsCond.jsx
+++ src/pages/TermsCond.jsx
@@ -1,16 +1,47 @@
+import { useState } from 'react';
 import '../css/pageBase.css'
 import Card from '../components/Card.jsx'
+import '../css/TermsCond.css'
 
 export default function TermsCond() {
+
+    const [terms,setTerms] = useState("Lorem ipsum dolor sit, amet consectetur adipisicing elit...");
+    const [isEditing,setIsEditing] = useState(false);
+    const [draftTerms,setDraftTerms] = useState(terms);
+
+    function editing(){
+        setDraftTerms(terms);
+        setIsEditing(true);
+    }
+
+    function cancel(){
+        setDraftTerms(terms);
+        setIsEditing(false);
+    }
+
+    function save(){
+        setTerms(draftTerms);
+        setIsEditing(false);
+    }
+    
     return (
         <div className="page">
             <div className='header'>
                 <h1>Términos y Condiciones</h1>
-                <button>Actualizar T&C</button>
+                {!isEditing ? (<button onClick={editing}>Actualizar T&C</button>) : 
+                (<div>
+                    <button className='green' onClick={save}>Guardar</button>
+                    <button className='red' onClick={cancel}>X</button>
+                </div>)}
             </div>
             <div className="grid">
                 <Card title='Actuales' size={[1,4]}>
-                
+                    {!isEditing ? (<p>{terms}</p>) : 
+                    (<textarea 
+                        className='edit-text' 
+                        value={draftTerms} 
+                        onChange={e => setDraftTerms(e.target.value)}
+                        rows={30}/>)}
                 </Card>
             </div>
         </div>
