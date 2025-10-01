829d225b3d9fce4905b90411f2ed7b29f9da7ec9
Rodrigo Martínez Vallejo <rodrigo.martinezvall@gmail.com>
2025-10-01 12:27:04 -0600

    refactored states in reportes

--- src/components/PaginationReportes.jsx
+++ src/components/PaginationReportes.jsx
@@ -14,35 +14,33 @@ export default function PaginationReportes({ rows, categorias }) {
     };
    
     //STATES FOR EDIT REPORTE
-    const [titleField,setTitlteField] = useState("");
-    const [selectedCategories,setSelectedCategories] = useState([]);
-    const [descriptionField,setDescriptionField] = useState("");
-    const [linkField,setLinkField] = useState("");
-    const [state,setState] = useState(1);
-    const [user,setUser] = useState("");
+    const [reportInfo,setReportInfo] = useState({
+        id: 1,
+        title: "",
+        image: "",
+        categories: [],
+        description: "",
+        link: "",
+        status: 1,
+        user: ""
+    });
+    
+    function handleSetReportInfo(report) {
+            setReportInfo({
+                id: report.id,
+                title: report.title,
+                image: report.image,
+                categories: report.categories,
+                description: report.description,
+                link: report.report_url,
+                status: report.status,
+                user: report.user_name
+            });
+        }
 
     const [isEditReportOpen,setIsEditReportOpen] = useState(false);
     const [isViewReportOpen,setIsViewReportOpen] = useState(false);
     const [isDeleteOpen,setIsDeleteOpen] = useState(false);
-    
-    function viewReport(report) {
-        setIsViewReportOpen(true);
-        setTitlteField(report.title);
-        setSelectedCategories(report.categories);
-        setDescriptionField(report.description);
-        setLinkField(report.report_url);
-        setState(report.status);
-        setUser(report.user_name);
-        console.log(report.title);
-    }
-
-    function editReport() {
-        setIsEditReportOpen(true);
-    }
-
-    function deleteReport() {
-
-    }
 
     return (
         <>
@@ -72,14 +70,21 @@ export default function PaginationReportes({ rows, categorias }) {
                             <td>{row.created_at}</td>
                             <td className='actions'>
                                 <button onClick={()=>{
-                                    viewReport(row);
+                                    handleSetReportInfo(row);
+                                    setIsViewReportOpen(true);
                                 }}>
                                     <img src='/icons/view.svg'/>
                                 </button>
-                                <button onClick={editReport}>
+                                <button onClick={()=>{
+                                    handleSetReportInfo(row);
+                                    setIsEditReportOpen(true);
+                                }}>
                                     <img src='/icons/edit.svg'/>
                                 </button>
-                                <button onClick={()=>setIsDeleteOpen(true)}>
+                                <button onClick={()=>{
+                                    handleSetReportInfo(row);
+                                    setIsDeleteOpen(true);
+                                }}>
                                     <img src='/icons/delete.svg'/>
                                 </button>
                             </td>
@@ -88,29 +93,21 @@ export default function PaginationReportes({ rows, categorias }) {
                 </tbody>
 
                 {isEditReportOpen && 
-                    <Window title='Editar Reporte' onClose={()=>{
-                        setTitlteField("");
-                        setSelectedCategories([]);
-                        setDescriptionField("");
-                        setIsEditReportOpen(false);
-                        setLinkField("");
-                        setState(1);
-                        }}>
+                    <Window title='Editar Reporte' onClose={()=>{setIsEditReportOpen(false);}}>
                         <div className="window-layout">
                             <div className="text-holder edit-report-text">
                                 <h4>Titulo del Reporte</h4>
-                                <input placeholder='Titulo' value={titleField} onChange={e => setTitlteField(e.target.value)}/>
-                                <p className='user-holder'>Nombre Usuario</p>
+                                <input placeholder='Titulo' value={reportInfo.title} onChange={e => setReportInfo(prev => ({...prev, title: e.target.value}))}/>
+                                <p className='user-holder'>{reportInfo.user}</p>
                                 <h4>Categorias</h4>
                                 <select className='toggle-select' onChange={e => {
-                                    setSelectedCategories(prevItems => {
+                                    setReportInfo(prev => {
                                         if (e.target.value == 0)
-                                            return [...prevItems];
-                                        if (!prevItems.includes(e.target.value))
-                                            return [...prevItems,e.target.value];
-                                        return [...prevItems];
+                                            return {...prev}
+                                        if (!prev.categories.includes(e.target.value))
+                                            return {...prev, categories: [...prev.categories, e.target.value]}
+                                        return {...prev}
                                     });
-                                    
                                 }}>
                                     <option value={0}>Selecionar Cateogiras</option>
                                     {
@@ -120,36 +117,40 @@ export default function PaginationReportes({ rows, categorias }) {
                                     }
                                 </select>
                                 <div className='categories-list'>
-                                    {selectedCategories.map(id => {
-                                        return <CategoryTag 
+                                    {
+                                        reportInfo.categories.map(id => {
+                                            return <CategoryTag 
                                                 key={id} 
                                                 categoryName={categorias[id-1].name} 
                                                 onDelete={() => {
-                                                    setSelectedCategories(
-                                                        (prevItems) => prevItems.filter(el => el !== id)
+                                                    setReportInfo(prev =>{
+                                                        return {...prev, categories: prev.categories.filter(el => el !== id)}
+                                                    }
+                                                        
                                                     )
                                                 }}
                                             />
-                                    })}
+                                        })
+                                    }
                                 </div>
                                 <h4>Descripción del reporte</h4>
                                 <textarea 
                                     className='edit-text' 
-                                    value={descriptionField} 
-                                    onChange={e => setDescriptionField(e.target.value)}
+                                    value={reportInfo.description} 
+                                    onChange={e => setReportInfo(prev => {return {...prev, description: e.target.value}})}
                                     rows={5}/>
                                 <div className='liga-holder'>
                                     <h4>Liga Fraudulenta</h4>
-                                    <input placeholder='https://ejemplo.com' value={linkField} onChange={e => setLinkField(e.target.value)}/>
+                                    <input placeholder='https://ejemplo.com' value={reportInfo.link} onChange={e => setReportInfo(prev => {return {...prev, link: e.target.value}})}/>
                                 </div>
                                 <div className='categories-list report-state'>
-                                    <button onClick={()=>setState(2)} className={`tag aceptado ${state === 2 ? 'selected' :''}`}>Aceptado</button>
-                                    <button onClick={()=>setState(3)} className={`tag rechazado ${state === 3 ? 'selected' : ''}`}>Rechazado</button>
-                                    <button onClick={()=>setState(1)} className={`tag revision ${state === 1 ? 'selected' : ''}`}>Pendiente</button>
+                                    <button onClick={()=>setReportInfo(prev => {return {...prev, status: 2}})} className={`tag aceptado ${reportInfo.status === 2 ? 'selected' :''}`}>Aceptado</button>
+                                    <button onClick={()=>setReportInfo(prev => {return {...prev, status: 3}})} className={`tag rechazado ${reportInfo.status === 3 ? 'selected' : ''}`}>Rechazado</button>
+                                    <button onClick={()=>setReportInfo(prev => {return {...prev, status: 1}})} className={`tag revision ${reportInfo.status === 1 ? 'selected' : ''}`}>Pendiente</button>
                                 </div>
                             </div>
                             <div className="image-holder">
-                                <img className='report-image' src="/prueba.jpg"/>
+                                <img className='report-image' src={reportInfo.image}/>
                             </div>
                         </div>
                         <div className='save-changes'>
@@ -164,28 +165,28 @@ export default function PaginationReportes({ rows, categorias }) {
                     <Window title='Reporte' onClose={()=>{setIsViewReportOpen(false)}}>
                         <div className='window-layout'>
                             <div className='text-holder'>
-                                <h3>{titleField}</h3>
-                                <p className='user-holder'>{user}</p>
+                                <h3>{reportInfo.title}</h3>
+                                <p className='user-holder'>{reportInfo.user}</p>
                                 <div className='categories-list'>
-                                    {selectedCategories.map(selectedCategory => {
+                                    {reportInfo.categories.map(selectedCategory => {
                                         return (<p key={categorias[selectedCategory-1].name} className='tag'>{categorias[selectedCategory-1].name}</p>);
                                     })}
                                 </div>
-                                <p>{descriptionField}</p>
+                                <p>{reportInfo.description}</p>
                                 <div className='liga-holder'>
                                     <h4>Liga Fraudulenta</h4>
-                                    <p>{linkField}</p>
+                                    <p>{reportInfo.link}</p>
                                 </div>
                                 <div className='report-state'>
                                     {
-                                        state === 1 ? <p className='tag revision'>Pendiente</p> :
-                                        state === 2 ? <p className='tag aceptado'>Aprobado</p> : 
+                                        reportInfo.status === 1 ? <p className='tag revision'>Pendiente</p> :
+                                        reportInfo.status === 2 ? <p className='tag aceptado'>Aprobado</p> : 
                                         <p className='tag rechazado'>Aceptado</p>
                                     }
                                 </div>
                             </div>
                             <div className='image-holder'>
-                                <img className='report-image' src="/prueba.jpg"/>
+                                <img className='report-image' src={reportInfo.image}/>
                             </div>
                         </div>
                     </Window>
@@ -194,7 +195,7 @@ export default function PaginationReportes({ rows, categorias }) {
                 {isDeleteOpen && 
                     <Window title='Eliminar Reporte' onClose={()=>setIsDeleteOpen(false)}>
                         <div className='delete-report'>
-                            <p>¿Seguro que desea eliminar el reporte con ID X?</p>
+                            <p>¿Seguro que desea eliminar el reporte con ID {reportInfo.id}?</p>
                             <button onClick={()=>{
                                 //llamado a funcion para eliminar reporte
 
