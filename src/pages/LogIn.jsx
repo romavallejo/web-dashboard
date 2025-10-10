5b26f627762c314fe5272ef842e7c89a7b58cf25
Rodrigo Martínez Vallejo <rodrigo.martinezvall@gmail.com>
2025-10-10 13:42:10 -0600

    started working on Login.jsx appearance

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -15,22 +15,24 @@ export default function LogIn() {
     }
 
     return (
-        <form className="login" onSubmit={dataSubmit}>
-            <h1>Iniciar Sesión</h1>
-            {error && <p>Error</p>}
-            <div className="field">
-                <p>Email:</p>
-                <input type="text" value={email} onChange={e => setEmail(e.target.value)}/>
+        <div className="login-holder finisher-header">
+            <div className="login" onSubmit={dataSubmit}>
+                <h1>Iniciar Sesión</h1>
+                {error && <p>Error</p>}
+                <div className="field">
+                    <p>Email:</p>
+                    <input type="text" value={email} onChange={e => setEmail(e.target.value)}/>
+                </div>
+                <div className="field">
+                    <p>Contraseña:</p>
+                    <input type="password" value={password} onChange={e => setPassword(e.target.value)}/>
+                </div>
+                <button className="submit" type="submit">Ingresar</button>
+                <div>
+                    <img className="logo"src='/logo.png' alt='Logo'/>
+                </div>
             </div>
-            <div className="field">
-                <p>Contraseña:</p>
-                <input type="password" value={password} onChange={e => setPassword(e.target.value)}/>
-            </div>
-            <button className="submit" type="submit">Ingresar</button>
-            <div>
-                <img className="logo"src='/logo.png' alt='Logo'/>
-            </div>
-        </form>
+        </div>
     );
 
 };
