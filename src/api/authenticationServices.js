3161ffbcf62bf7bdd6fdcff9ce2f91622b029f2a
Arturo Utrilla Hernández <itsart.tw@gmail.com>
2025-10-11 13:09:45 -0600

    ya pelon

--- src/api/authenticationServices.js
+++ src/api/authenticationServices.js
@@ -1,35 +1,40 @@
 const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
 export async function authenticateCredentials(e, p) {
-    console.log(JSON.stringify({
-            email: e,
-            password: p,
-            type: "web"
-        }))
+  console.log(
+    JSON.stringify({
+      email: e,
+      password: p,
+      type: "web",
+    })
+  );
 
-    const res = await fetch(`${BASE_URL}/auth/login`,{
-        method: "POST",
-        //headers:  {"Content-Type": "application/json"},
-        body: JSON.stringify({
-            email: e,
-            password: p,
-            type: "web"
-        })   
-    });
+  const res = await fetch(`${BASE_URL}/auth/login`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      email: e,
+      password: p,
+      type: "web",
+    }),
+  });
 
-    const data = await res.json();
-    if (!res.ok) throw new Error(data?.message || "Login Failed");
+  if (!res.ok) throw new Error(data?.message || "Login Failed");
+  const data = await res.json();
 
-    return {accessToken: response.data.accessToken,refreshToken: response.data.refreshToken};
+  return { accessToken: data.accessToken, refreshToken: data.refreshToken };
 }
 
 export async function refreshAccessToken(refToken) {
-    const res = await fetch(`${BASE_URL}/auth/refresh`,{
-        refreshToken: refToken
-    });
+  const res = await fetch(`${BASE_URL}/auth/refresh`, {
+    refreshToken: refToken,
+  });
 
-    if (!res.ok) throw new Error(data?.message || "Refresh Failed");
+  if (!res.ok) throw new Error(data?.message || "Refresh Failed");
 
-    if (res.status === 201)
-        return {accessToken: res.data.accessToken,refreshToken: res.data.refreshToken}
+  if (res.status === 201)
+    return {
+      accessToken: res.data.accessToken,
+      refreshToken: res.data.refreshToken,
+    };
 }
